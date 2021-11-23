8c259f7cadf147c1fc3850cde044483c776b16bf
holivieri <hernan.olivieri@gmail.com>
2021-11-23 15:33:53 -0800

    user bloc

---
+++ undercover_mobile/lib/src/blocs/users/users_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../models/user_model.dart';
+import '../../repositories/user_repository.dart';
+
+part 'users_event.dart';
+part 'users_state.dart';
+
+class UsersBloc extends Bloc<UsersEvent, UsersState> {
+  UsersBloc(this.userRepository) : super(UsersInitial()) {
+    on<ValidateUser>((event, emit) async {
+      final user = await userRepository.login(event.userName, event.password);
+      if (user is User) {
+        emit(UserIsValidated(user));
+      } else {
+        emit(ValidateError(user.toString()));
+      }
+    });
+  }
+
+  final UserRepository userRepository;
+}
